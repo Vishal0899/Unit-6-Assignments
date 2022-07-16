5ada3c8368aa61d597cb3b0488c64a9befbf40c8
Vishal0899 <97458281+Vishal0899@users.noreply.github.com>
2022-07-16 19:04:55 +0530

    commited

--- RazorPay/Raazor_Pay_Frontend/src/App.jsx
+++ RazorPay/Raazor_Pay_Frontend/src/App.jsx
@@ -1,34 +1,33 @@
 import { useState, useEffect } from "react";
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [orderAmount,setOrderAmount] = useState(false)
-  const [orders, setOrders] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [orderAmount, setOrderAmount] = useState(false);
+  const [orders, setOrders] = useState([]);
   async function fetchOrders() {
-    const { data } = await axios.get('/list-orders')
-    setOrders(data)
+    const { data } = await axios.get("/orders/list");
+    setOrders(data);
   }
 
   useEffect(() => {
-    fetchOrders()
-  },[])
+    fetchOrders();
+  }, []);
   function loadRazorpay() {
-    let script = document.createElement('script');
+    let script = document.createElement("script");
     script.src = "https://checkout.razorpay.com/v1/checkout.js";
   }
   script.onerror = async () => {
-    alert("Razorpay SDK failed to load. Are you online?")
+    alert("Razorpay SDK failed to load. Are you online?");
   };
   script.onload = async () => {
     try {
       setLoading(true);
-      const result = await axios.post("/create-order", {
-        amount: orderAmount + "00"
+      const result = await axios.post("/orders/create", {
+        amount: orderAmount + "00",
       });
-      const { amount, id, order_id, currency } = result.data
+      const { amount, id, order_id, currency } = result.data;
       const {
         data: { key: razorpay },
       } = axios.get(`get-razor-key`);
@@ -40,7 +39,7 @@ function App() {
         description: "example description ",
         order_id: order_id,
         handler: async function (res) {
-          const result = await axios.post("/pay-order", {
+          const result = await axios.post("/order/pay", {
             amount: amount,
             razorpayPaymentId: res.razorpay_payment_id,
             razorpayOrderId: res.razorpay_order_id,
@@ -50,9 +49,9 @@ function App() {
           fetchOrders();
         },
         prefill: {
-          name: "Ajay",
-          email: "ajay@example.com",
-          contact: "9999999999",
+          name: "Vishal",
+          email: "vishal@gmail.com",
+          contact: "7896541235",
         },
         notes: {
           address: "Razorpay Corporate Office",
@@ -61,26 +60,31 @@ function App() {
           color: "#3399cc",
         },
       };
-      setLoading(false)
-      const paymentObject = new window.Razorpay(options)
+      setLoading(false);
+      const paymentObject = new window.Razorpay(options);
     } catch (error) {
       alert(err);
-      setLoading(false)
+      setLoading(false);
     }
-    document.body.appendChild(script)
-  }
+    document.body.appendChild(script);
+  };
   return (
     <div className="App">
       <h1>Razorpay Implimantation Demo</h1>
       <hr />
       <h3>Demo Order</h3>
       <label htmlFor="">
-        Amount : { }
-        <input type="number" placeholder="INR" value={orderAmount}
-          onChange={(e)=>setOrderAmount(e.target.value)}
+        Amount : {}
+        <input
+          type="number"
+          placeholder="INR"
+          value={orderAmount}
+          onChange={(e) => setOrderAmount(e.target.value)}
         />
       </label>
-        <button disabled={loading} onClick={loadRazorpay}>Razorpay</button>
+      <button disabled={loading} onClick={loadRazorpay}>
+        Razorpay
+      </button>
       {loading && <div>Loading...</div>}
       <div className="list-orders">
         <h2>Orders</h2>
@@ -94,21 +98,19 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {
-              orders.map(elem => {
-                <tr key={elem.id}>
-                  <td>{elem.id}</td>
-                  <td>{elemamount/100}</td>
-                  <td>{elem.isPaid}</td>
-                  <td>{elem.razorpay.paymentId}</td>
-                </tr>;
-              })
-            }
+            {orders.map((elem) => {
+              <tr key={elem.id}>
+                <td>{elem.id}</td>
+                <td>{elemamount / 100}</td>
+                <td>{elem.isPaid}</td>
+                <td>{elem.razorpay.paymentId}</td>
+              </tr>;
+            })}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- RazorPay/server.js
+++ RazorPay/server.js
@@ -1,78 +1,77 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const Razorpay = require("razorpay")
-// const dotenv = require("dotenv")
+const express = require("express");
+const mongoose = require("mongoose");
+const Razorpay = require("razorpay");
 require("dotenv").config();
-// dotenv.config()
 
-const app = express()
+const app = express();
 
-mongoose.connect(process.env.MONGO_URI).then(() => { console.log("mongoDB Connected") })
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("mongoDB Connected");
+});
 
-app.use(express.json())
+app.use(express.json());
 
 const OrderSchema = mongoose.Schema({
-    isPaid: Boolean,
-    amount: Number,
-    razorpay: {
-        orderId: String,
-        paymentId: String,
-        signature : String
-    }
-})
+  isPaid: Boolean,
+  amount: Number,
+  razorpay: {
+    orderId: String,
+    paymentId: String,
+    signature: String,
+  },
+});
 
-const Order = mongoose.model("Order", OrderSchema)
+const Order = mongoose.model("Order", OrderSchema);
 
 app.get("/get-razorpay-key", (req, res) => {
-    res.send({key:process.env.RAZORPAY_KEY_ID})
-})
+  res.send({ key: process.env.RAZORPAY_KEY_ID });
+});
 
-app.post("/create-order", async (req, res, next) => {
-    try {
-        const instance = new Razorpay({
-          key_id: process.env.RAZORPAY_KEY_ID,
-          key_secret: process.env.RAZORPAY_KEY_SECRET,
-        });
-        const options = {
-            amount: req.body.amount,
-            currency: "INR"
-        };
-        const order = await instance.orders.create(options)
-        if (!order) return res.status(500).send("Some Errors Occured")
-        res.send(order)
+app.post("/orders/create", async (req, res, next) => {
+  try {
+    const instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+    const options = {
+      amount: req.body.amount,
+      currency: "INR",
+    };
+    const order = await instance.orders.create(options);
+    if (!order) return res.status(500).send("Some Errors Occured");
+    res.send(order);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
+app.post("/orders/pay", async (req, res) => {
+  try {
+    const { amount, razorPaymentId, razorpayOrderId, razorpaySignature } =
+      req.body;
+    const newPayment = Order({
+      isPaid: true,
+      amount: amount,
+      razorpay: {
+        orderId: razorPaymentId,
+        paymentId: razorpayOrderId,
+        signature: razorpaySignature,
+      },
+    });
+    await newPayment.save();
+    res.send({ message: "Payment was suscessfull" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-app.post("/pay-order",async(req,res)=> {
-    try {
-        const { amount, razorPaymentId, razorpayOrderId, razorpaySignature } = req.body;
-        const newPayment = Order({
-          isPaid: true,
-          amount: amount,
-          razorpay: {
-            orderId: razorPaymentId,
-            paymentId: razorpayOrderId,
-            signature: razorpaySignature,
-          },
-        });
-        await newPayment.save();
-        res.send({message : "Payment was suscessfull"})
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-app.get("/list-orders", async (req, res) => {
-    const orders = await Order.find();
-    res.send(orders)
-
-})
+app.get("/orders/list", async (req, res) => {
+  const orders = await Order.find();
+  res.send(orders);
+});
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server running at ${port}`)
-})
+  console.log(`Server running at ${port}`);
+});
